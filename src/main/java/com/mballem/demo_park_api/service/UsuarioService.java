a91ce81fcb123158237e5c6509bc7744f575000e
Antonio André <antonioandre1008@gmail.com>
2024-06-18 13:57:54 -0500

    feat: inclui buscar todos e deletar

--- src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -30,4 +32,15 @@ public class UsuarioService {
         return user;
     }
 
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    @Transactional
+    public Usuario deletarPorId(Long id) {
+        Usuario user = buscarPorId(id);
+        usuarioRepository.delete(user);
+        return user;
+    }
 }
--- src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/usuarios")
 @RequiredArgsConstructor
@@ -29,6 +31,18 @@ public class UsuarioController {
     @PatchMapping("/{id}")
     public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
         Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Usuario> delete(@PathVariable Long id) {
+       Usuario user = usuarioService.deletarPorId(id);
         return ResponseEntity.ok(user);
     }
 
