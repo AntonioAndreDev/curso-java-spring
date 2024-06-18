e864f3214ddd1f3cc64458ae03b8f6b368a019aa
Antonio André <antonioandre1008@gmail.com>
2024-06-18 15:25:47 -0500

    feat(validacao): cria validacao de username e password

--- src/main/java/com/mballem/demo_park_api/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/mballem/demo_park_api/web/dto/UsuarioCreateDto.java
@@ -1,5 +1,6 @@
 package com.mballem.demo_park_api.web.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -8,6 +9,12 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class UsuarioCreateDto {
+
+    @NotBlank
+    @Email(message = "Formato do email inválido")
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
