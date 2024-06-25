4c7f34752826ed5176d6eaa7e2201958cc4b8add
Antonio André <antonioandre1008@gmail.com>
2024-06-25 08:55:38 -0500

    feat: USER pode visualizar apenas os dados do seu perfil

--- src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
@@ -68,7 +68,7 @@ public class UsuarioController {
             }
     )
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN') OR (hasRole('USER') AND #id == authentication.principal.id)")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
