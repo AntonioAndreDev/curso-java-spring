10f7b30b3d73c42bbbce08c45f2ca9afef166cb3
Antonio André <antonioandre1008@gmail.com>
2024-06-27 11:16:50 -0500

    feat: altera insert para senha criptografada
    Como agora está trabalhando com senha criptografada, ao fazer a autenticação a API espera por uma senha criptografada para autenticar o usuário

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,6 @@
-insert into USUARIOS (id, username, password, role) values (100, 'antonio@gmail.com', '123456', 'ROLE_ADMIN');
-insert into USUARIOS (id, username, password, role) values (101, 'giuli@gmail.com', '123456', 'ROLE_USER');
-insert into USUARIOS (id, username, password, role) values (102, 'toin@gmail.com', '123456', 'ROLE_USER');
+insert into USUARIOS (id, username, password, role)
+values (100, 'antonio@gmail.com', ' $2a$12$cGPtPrlVEoscxK/JNqgCw.JnfOzYl5kqP9jRLWGeMy3CzyShZHNGW ', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role)
+values (101, 'giuli@gmail.com', ' $2a$12$cGPtPrlVEoscxK/JNqgCw.JnfOzYl5kqP9jRLWGeMy3CzyShZHNGW ', 'ROLE_USER');
+insert into USUARIOS (id, username, password, role)
+values (102, 'toin@gmail.com', ' $2a$12$cGPtPrlVEoscxK/JNqgCw.JnfOzYl5kqP9jRLWGeMy3CzyShZHNGW ', 'ROLE_USER');
