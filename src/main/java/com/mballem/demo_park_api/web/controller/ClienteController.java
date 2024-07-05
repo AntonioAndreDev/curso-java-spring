b7fd248ae69ad4ed67792e1d6b7a60d2a588ef17
Antonio André <antonioandre1008@gmail.com>
2024-07-04 20:27:09 -0500

    feat(controller): cria metodo que cria um cliente

---
+++ src/main/java/com/mballem/demo_park_api/web/controller/ClienteController.java
@@ -0,0 +1,39 @@
+package com.mballem.demo_park_api.web.controller;
+
+
+import com.mballem.demo_park_api.entity.Cliente;
+import com.mballem.demo_park_api.jwt.JwtUserDetails;
+import com.mballem.demo_park_api.service.ClienteService;
+import com.mballem.demo_park_api.service.UsuarioService;
+import com.mballem.demo_park_api.web.dto.ClienteCreateDto;
+import com.mballem.demo_park_api.web.dto.ClienteResponseDto;
+import com.mballem.demo_park_api.web.dto.mapper.ClienteMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/clientes")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<ClienteResponseDto> create(@RequestBody @Valid ClienteCreateDto dto,
+                                                     @AuthenticationPrincipal JwtUserDetails userDetails) {
+        Cliente cliente = ClienteMapper.toCliente(dto);
+        cliente.setUsuario(usuarioService.buscarPorId(userDetails.getId()));
+        clienteService.salvar(cliente);
+        return ResponseEntity.status(201).body(ClienteMapper.toDto(cliente));
+    }
+
+}
