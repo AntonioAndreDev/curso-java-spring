a8e25ab2df4d46df91d145a55d58d196e28a3438
Antonio André <antonioandre1008@gmail.com>
2024-07-15 09:23:18 -0500

    feat: implementa VagaService

--- src/main/java/com/mballem/demo_park_api/repository/VagaRepository.java
+++ src/main/java/com/mballem/demo_park_api/repository/VagaRepository.java
@@ -2,6 +2,11 @@ package com.mballem.demo_park_api.repository;
 
 import com.mballem.demo_park_api.entity.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
+    Optional<Vaga> findByCodigo(String codigo);
 }
