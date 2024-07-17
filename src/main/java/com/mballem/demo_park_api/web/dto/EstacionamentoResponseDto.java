2edce5c64af5a880d6bdac721233bc8cbb3a73c4
Antonio André <antonioandre1008@gmail.com>
2024-07-17 10:51:11 -0500

    feat: cria DTOs e Mapper para recurso de estacionamentos

---
+++ src/main/java/com/mballem/demo_park_api/web/dto/EstacionamentoResponseDto.java
@@ -0,0 +1,39 @@
+package com.mballem.demo_park_api.web.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EstacionamentoResponseDto {
+    private String placa;
+
+    private String marca;
+
+    private String modelo;
+
+    private String cor;
+
+    private String clienteCpf;
+
+    private String recibo;
+
+    private LocalDateTime dataEntrada;
+
+    private LocalDateTime dataSaida;
+
+    private String vagaCodigo;
+
+    private BigDecimal valor;
+
+    private BigDecimal desconto;
+}
